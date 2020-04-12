d26ed82f2991a54ad62ef5dfcbf3bd1ef988d7b6
mateus rodrigues <mateus037@gmail.com>
2020-04-12 20:44:06 -0300

    dockerizando aplicação

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
 const requireDir = require('require-dir')
+const dotenv = require('dotenv')
+dotenv.config()
+
+const { DB_HOST, DB_PORT, DB_NAME } = process.env
+const {SERVER_PORT} = process.env
+
+const HOST = '0.0.0.0';
 
 // Iniciando o App
 const app = express()
@@ -9,7 +16,7 @@ app.use(express.json())
 app.use(cors())
 
 // Iniciando o DB
-mongoose.connect('mongodb://localhost:27017/nodeapi', {
+mongoose.connect(`mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
     useFindAndModify: false
@@ -19,4 +26,4 @@ requireDir('./src/models')
 // Rotas
 app.use('/api', require('./src/routes'))
 
-app.listen(3001)
+app.listen(SERVER_PORT, HOST)
